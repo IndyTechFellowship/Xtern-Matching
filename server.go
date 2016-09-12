b6e5b430b9dc82b3c6af666e35986a7d646aebfb
xniccum <xniccum@gmail.com>
2016-09-12 09:15:16 -0400

    altered and allowed app engine integration and deployment

--- server.go
+++ server.go
@@ -1,71 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-
-	"os"
-
-	"github.com/bmizerany/pat"
-	"github.com/codegangsta/negroni"
-	"github.com/spf13/viper"
+	"html/template"
 )
 
-var Settings map[string]*viper.Viper
-var environments string
-
-func newServer() *http.Server {
-	n := negroni.New()
-
-	// Middlewares
-	if Settings["environments"].GetBool("log") {
-		n.Use(negroni.NewLogger())
-	}
-
-	//n.UseFunc(recovery())
-	n.Use(negroni.NewStatic(http.Dir("./public")))
-
-	// Setup routes
-	router := pat.New()
-	//router.Get("/api/v1/jsonInfo", http.HandlerFunc(sampleHandler))
-
-	// Add alive endpoint
-	// router.Get("/alive", http.HandlerFunc(alive))
-	// Add the router action
-	n.UseHandler(router)
-	Server := &http.Server{
-		Addr:           ":" + Settings["environments"].GetString("server.port"),
-		Handler:        n,
-		MaxHeaderBytes: 1 << 20,
-	}
-	return Server
-}
-
-func InitConfigs() {
-	environments = os.Getenv("ENV")
-	if environments == "" {
-		environments = "development"
-	}
-	Settings = make(map[string]*viper.Viper)
-	Settings["environments"] = viper.New()
-	Settings["environments"].SetConfigType("yml")
-	Settings["environments"].AddConfigPath("config/environments")
-	Settings["environments"].SetConfigName(environments)
-	err := Settings["environments"].ReadInConfig()
-	if err != nil {
-		fmt.Errorf("Fatal error config file: %s \n", err)
-	}
-}
-
-func main() {
-	InitConfigs()
-	InitApp()
-	Server := newServer()
-	log.Printf("Start serving on %s", Server.Addr)
-	log.Println(Server.ListenAndServe())
-}
-
-func InitApp() {
-	// add here plugins and middleware
+func init() {
+	//main()
+	http.HandleFunc("/",func(w http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("public/index.html")  // Parse template file.
+		t.Execute(w, nil)  // merge.
+	})
 }
