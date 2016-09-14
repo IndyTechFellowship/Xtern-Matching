f055264b8f55628f372f15d7cf5ebbdf213c14cb
xniccum <xniccum@gmail.com>
2016-09-14 14:04:19 -0400

    removed unnessisary lines from server

--- server.go
+++ server.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"net/http"
-	"html/template"
-	//"appengine"
-	//"appengine/user"
 	"Xtern-Matching/routes"
 )
 
 func init() {
 	http.Handle("/",routes.NewRouter())
-
-	http.HandleFunc("/",func(w http.ResponseWriter, req *http.Request) {
-		t, _ := template.ParseFiles("public/index.html")
-		t.Execute(w, nil)
-	})
 }
