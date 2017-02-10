6a7062ec62139a35184ee56ef0b32f2346d23808
crowleaj <ajc927@hotmail.com>
2017-02-10 03:10:55 -0500

    Changed form Aliases from map to array.

--- models/form.go
+++ models/form.go
@@ -4,5 +4,5 @@ type Form struct {
 	Name string 			`json:"name"`
 	Year string			`json:"year"`
 	Active bool			`json:"active"`
-	//TODO: Support aliases Aliases map[string]string 	`json:"aliases" datastore:",noindex"`
+	Aliases []string		`json:"aliases"`
 }
