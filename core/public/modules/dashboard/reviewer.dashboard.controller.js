4413eb0969d15c120b544027a686c96654d18f8b
Doolan <doolansr@rose-hulman.edu>
2017-02-13 00:17:36 -0500

    Sortable Table Headers

--- core/public/modules/dashboard/reviewer.dashboard.controller.js
+++ core/public/modules/dashboard/reviewer.dashboard.controller.js
@@ -6,6 +6,11 @@ angular.module('Xtern').controller('ReviewerDashboardCtrl', function($scope, $st
     $scope.rawData = null;
     $scope.personsCount = 0;
 
+    $scope.tableHeaders = [
+        {title: 'Name', displayProperty:'name', sortPropertyName: 'name', asc: true},
+        {title: 'Your Grade', displayProperty:'currentReviewerGrade', sortPropertyName: 'sortReviewerGrade', asc: true}
+    ];
+
     $scope.rowClick = function (key) {
         $state.go(PATH + '.profile', {key: key});
     };
@@ -14,8 +19,12 @@ angular.module('Xtern').controller('ReviewerDashboardCtrl', function($scope, $st
         var students = [];
         for(var i = 0; i < data.length; i++) {
             students[i] = rowClass(data[i],keys[i]);
+            students[i].name = students[i].firstName + " " + students[i].lastName;
             if(grades[i] !== null && grades[i] > 0) {
                 students[i].currentReviewerGrade = grades[i];
+                students[i].sortReviewerGrade = grades[i];
+            }else if(grades[i]){
+                students[i].sortReviewerGrade = -1;
             }
         }
         $scope.summaryData = students;
@@ -24,4 +33,20 @@ angular.module('Xtern').controller('ReviewerDashboardCtrl', function($scope, $st
         $('.ui.dropdown').dropdown();//activates semantic drowpdowns
         $('.ui.accordion').accordion();
     });
+
+    //Standard sort function
+    $scope.sort = function (header, event) {
+        var prop = header.sortPropertyName;
+        var asc = header.asc;
+        header.asc = !header.asc;
+        var ascSort = function (a, b) {
+            return a[prop] < b[prop] ? -1 : a[prop] > b[prop] ? 1 : 0;
+        };
+        var descSort = function (a, b) {
+            return ascSort(b, a);
+        };
+        var sortFunc = asc ? ascSort : descSort;
+        $scope.summaryData.sort(sortFunc);
+    };
+
 });
